384388973d558c4d4c5896ffab3cdc7df2274c8b
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-09-29 13:57:52 -0400

    Add on change text methods to register form

--- App/Components/user_register_form.js
+++ App/Components/user_register_form.js
@@ -8,28 +8,39 @@ import { StyleSheet,
 class RegisterForm extends Component {
   constructor() {
     super();
-    this.state = {
+    this.state = {user: {
       firstName: "First Name",
       lastName: "Last Name",
       email: "email",
       password: "password"
-    };
+    }};
   }
+
+  handleInputChange(name, text) {
+    const user = this.state.user;
+    user[name] = text;
+    this.setState({user: user})
+  }
+
+  onChangeFirstName = this.handleInputChange.bind(this, "firstName")
+  onChangeLastName = this.handleInputChange.bind(this, "lastName")
+  onChangeEmail = this.handleInputChange.bind(this, "email")
+  onChangePassword = this.handleInputChange.bind(this, "password")
 
   render() {
     return(
       <View>
         <Text>Register</Text>
         <Text>First Name:</Text>
-        <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}} value={this.state.firstName}/>
+        <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}} value={this.state.user.firstName} onChangeText={this.onChangeFirstName}/>
         <Text>Last Name:</Text>
-        <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}} value={this.state.lastName}/>
+        <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}} value={this.state.user.lastName} onChangeText={this.onChangeLastName}/>
         <Text>Email:</Text>
         <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-      value={this.state.email}/>
+      value={this.state.user.email} onChangeText={this.onChangeEmail}/>
         <Text>Password:</Text>
         <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-      value={this.state.password}/>
+      value={this.state.user.password} onChangeText={this.onChangePassword}/>
       </View>
     )
   }
--- index.ios.js
+++ index.ios.js
@@ -26,7 +26,7 @@ export default class splittr extends Component {
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
         </Text>
-        <LoginForm />
+        <RegisterForm />
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
