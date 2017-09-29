36f389a2f0991fe29eabf510a679fd836d108b50
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-09-29 14:20:47 -0400

    Add onchangetext methods to expense form

--- App/Components/new_expense_form.js
+++ App/Components/new_expense_form.js
@@ -7,14 +7,22 @@ import { TextInput,
 class ExpenseForm extends Component {
   constructor() {
     super();
-    this.state = {
+    this.state = {expense: {
       description: "description",
       amount: "$12.34",
-      vendor: "Rico's Seaside Bar & Grill",
-    }
+      vendor: "Rico's Seaside Bar & Grill"
+    }}
   }
 
-  
+  handleInputChange(name, text) {
+    const expense = this.state.expense;
+    expense[name] = text;
+    this.setState({expense: expense})
+  }
+
+  onChangeDescription = this.handleInputChange.bind(this, "description")
+  onChangeAmount = this.handleInputChange.bind(this, "amount")
+  onChangeVendor = this.handleInputChange.bind(this, "vendor")
 
   render() {
     return(
@@ -22,13 +30,13 @@ class ExpenseForm extends Component {
         <Text>Add New Expense</Text>
         <Text>Vendor:</Text>
         <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-      value={this.state.vendor}/>
+      value={this.state.expense.vendor} onChangeText={this.onChangeVendor}/>
         <Text>Description:</Text>
         <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-      value={this.state.description}/>
+      value={this.state.expense.description} onChangeText={this.onChangeDescription}/>
         <Text>Amount:</Text>
         <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-      value={this.state.amount}/>
+      value={this.state.expense.amount} onChangeText={this.onChangeAmount}/>
       </View>
     )
   }
