d0b754b08944009befd49ec3656764b77bfec374
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-09-29 13:44:05 -0400

    Add onChangeText method to the login form

--- App/Components/login_form.js
+++ App/Components/login_form.js
@@ -9,22 +9,31 @@ import { AppRegistry,
 class LoginForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      email: "placeholder",
-      password: "password"
-    };
+    this.state = {user: {
+      email: "email",
+      password: ""
+    }};
   }
+
+  handleInputChange(name, text) {
+    const user = this.state.user;
+    user[name] = text;
+    this.setState({user: user})
+  }
+
+  onChangeEmail = this.handleInputChange.bind(this, "email");
+  onChangePassword = this.handleInputChange.bind(this, "password")
 
   render() {
     return(
       <View>
         <Text>Login</Text>
         <Text>Email:</Text>
-        <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-      value={this.state.email}/>
+        <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}} name="email"
+      value={this.state.user.email} onChangeText={this.onChangeEmail}/>
         <Text>Password:</Text>
-        <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-      value={this.state.password}/>
+        <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}} name="password"
+      value={this.state.user.password} onChangeText={this.onChangePassword}/>
       </View>
     )
   }
