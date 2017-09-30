98364836705339c01fb903dc4a8e2e3ca2bca35b
Enrico De Santis <email": null>
2017-09-30 12:51:28 -0400

    copy and paste group show scene

--- App/Scenes/group_show.ios.js
+++ App/Scenes/group_show.ios.js
@@ -1,1 +1,20 @@
+import React, {Component} from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import  MemberList from '../Components/member_list';
 
+class GroupShowScene extends Component {
+  render() {
+    return(
+      <View>
+        <Text>group name</Text>
+        <MemberList />
+      </View>
+    )
+  }
+}
+export { GroupShowScene };
