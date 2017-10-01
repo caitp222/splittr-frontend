e05c6d5410a55ffff3581c8357c4b55fa342dcd1
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-10-01 18:03:29 -0400

    Update bundle

--- App/Components/profile_group_list.js
+++ App/Components/profile_group_list.js
@@ -19,8 +19,8 @@ class ProfileGroup extends Component {
         <Text> Your Groups </Text>
         {groups.map(function(group, index){
           return (
-            <TouchableHighlight>
-              <Text key={index} onPress = {() => navigation.navigate('GroupShow', {group_id: group.id})}>{group.group_name}: button</Text>
+            <TouchableHighlight key={index}>
+              <Text onPress = {() => navigation.navigate('GroupShow', {group_id: group.id})}>{group.group_name}: button</Text>
             </TouchableHighlight>
           )
         })}
