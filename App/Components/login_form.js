45e8b4220dfffae217496d11e3a238b76982e77d
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-10-01 10:33:40 -0400

    User id is added to asyncstorage on login

--- App/Components/login_form.js
+++ App/Components/login_form.js
@@ -6,6 +6,7 @@ import { AppRegistry,
          Button,
          View,
          Text,
+         AsyncStorage
        } from 'react-native';
 
 class LoginForm extends Component {
@@ -26,6 +27,27 @@ class LoginForm extends Component {
   onChangeEmail = this.handleInputChange.bind(this, "email");
   onChangePassword = this.handleInputChange.bind(this, "password")
 
+  handleHighLightPress() {
+    const { user } = this.state;
+    fetch("http://localhost:3000/sessions", {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({user})
+    }).then(function(response) {return response.json()} )
+    .then(function(responseJson) {
+      let sessionId = responseJson.id.toString()
+      AsyncStorage.setItem('sessionId', JSON.stringify(sessionId),
+    () => { AsyncStorage.getItem('sessionId', (err, result) => {
+      console.log(result)
+    }) } )
+      })
+  }
+
+  handleUserSubmit = this.handleHighLightPress.bind(this)
+
   render() {
     return(
       <View>
@@ -38,13 +60,13 @@ class LoginForm extends Component {
         <Text>Password:</Text>
         <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}} name="password"
       value={this.state.user.password} onChangeText={this.onChangePassword}/>
-
-      <TouchableHighlight >
-        <Text onPress = {"Placeholder"}>Login:button</Text>
-      </TouchableHighlight>
+        <TouchableHighlight >
+          <Text onPress = {this.handleUserSubmit}>Login:button</Text>
+        </TouchableHighlight>
       </View>
     )
   }
+
 }
 
 export { LoginForm };
--- App/Components/user_register_form.js
+++ App/Components/user_register_form.js
@@ -32,7 +32,7 @@ class RegisterForm extends Component {
 
   handleHighLightPress() {
     const { user } = this.state
-    fetch("http://localhost:3000/users", {
+    fetch("https://rocky-forest-46725.herokuapp.com/users", {
       method: 'post',
       headers: {
         'Accept': 'application/json',
