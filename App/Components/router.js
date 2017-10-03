b72153a1ac5281168c164cbbeff6cb657ceef149
Rebecca trachtenberg <rebecca.rose1836@yahoo.com>
2017-10-03 12:43:11 -0400

    simplified nav bar

--- App/Components/router.js
+++ App/Components/router.js
@@ -3,17 +3,10 @@ import { TabNavigator } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 
 import UserShowScene from '../Scenes/user_show.ios.js';
-import GroupShowScene from '../Scenes/group_show.ios.js';
+import GroupNewScene from '../Scenes/new_group.ios.js';
 import ExpensesShowScene from '../Scenes/expenses_show.ios.js'
 
 export const Tabs = TabNavigator({
-  GroupShowScene: {
-    screen: GroupShowScene,
-    navigationOptions: {
-      tabBarLabel: 'Groups',
-      tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />
-    },
-  },
   UserShowScene: {
     screen: UserShowScene,
     navigationOptions: {
@@ -21,11 +14,11 @@ export const Tabs = TabNavigator({
       tabBarIcon: ({ tintColor }) => <Icon name="account-circle" size={35} color={tintColor} />
     },
   },
-  ExpensesShowScene: {
-    screen: ExpensesShowScene,
+  GroupNewScene: {
+    screen: GroupNewScene,
     navigationOptions: {
-      tabBarLabel: 'Me',
-      tabBarIcon: ({ tintColor }) => <Icon name="home" size={35} color={tintColor} />
+      tabBarLabel: 'Create New Group',
+      tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />
     },
   },
 });
