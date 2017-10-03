8342a606c39c6654d19fa6a4409a8f6e226da759
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-10-03 10:26:55 -0400

    Don't display settle up button if group is settled up

--- App/Scenes/group_show.ios.js
+++ App/Scenes/group_show.ios.js
@@ -117,7 +117,7 @@ class GroupShowScene extends Component {
     group: {
       groupName: responseJson.group.group_name,
       details: responseJson.group.details,
-      settledUp: responseJson.group.settle_up
+      settledUp: responseJson.group.settled_up
     },
     totalSpend: responseJson.total_spend,
     memberSplit: responseJson.member_split,
@@ -132,7 +132,8 @@ class GroupShowScene extends Component {
     fetch(url + id + "/settle", {
       method: 'POST'
     }).then((response) => response.json()
-  ).then((responseJson) => console.log(responseJson))
+  ).then((responseJson) => alert(responseJson.message)
+    )
   }
 
   settleUpHandler = this.settleUpHandler.bind(this);
@@ -140,39 +141,71 @@ class GroupShowScene extends Component {
   render() {
     const { navigate } = this.props.navigation;
     const id = this.props.navigation.state.params.groupId;
-    return(
-      <LinearGradient colors={['#b6fbff', '#83a4d4']} style={styles.linearGradient}>
-        <View style={styles.container}>
-          <View>
+
+    if(this.state.group.settledUp === false){
+      return(
+        <LinearGradient colors={['#b6fbff', '#83a4d4']} style={styles.linearGradient}>
+          <View style={styles.container}>
             <View>
-              <Text style={styles.groupHeader}>{this.state.group.groupName}</Text>
-              <Text style={styles.details}>{this.state.group.details}</Text>
-            </View>
-            <View style={styles.sumHeader}>
-              <View style={styles.sumContainer}>
-                <View style={styles.sumBox}>
-                  <Text style={styles.expenseHeader}>Group Spent</Text>
-                  <Text>${this.state.totalSpend}</Text>
-                </View>
-                <View style={styles.sumBox}>
-                  <Text style={styles.expenseHeader}>Member Split</Text>
-                  <Text>${this.state.memberSplit}</Text>
-                </View>
+              <View>
+                <Text style={styles.groupHeader}>{this.state.group.groupName}</Text>
+                <Text style={styles.details}>{this.state.group.details}</Text>
               </View>
-              <TouchableHighlight style={styles.button} onPress={() => navigate('Expense')}>
-                <Text style={styles.buttonText}>Add Expense</Text>
-              </TouchableHighlight>
-            </View>
-            <MemberList groupId={id} members={this.state.members} navigate={ navigate }/>
-            </View>
-            <View style={styles.footer}>
-              <TouchableHighlight style={styles.settleButton} onPress={this.settleUpHandler}>
-                <Text style={styles.settleText}>Settle up</Text>
-              </TouchableHighlight>
-            </View>
-        </View>
-      </LinearGradient>
-    )
+              <View style={styles.sumHeader}>
+                <View style={styles.sumContainer}>
+                  <View style={styles.sumBox}>
+                    <Text style={styles.expenseHeader}>Group Spent</Text>
+                    <Text>${this.state.totalSpend}</Text>
+                  </View>
+                  <View style={styles.sumBox}>
+                    <Text style={styles.expenseHeader}>Member Split</Text>
+                    <Text>${this.state.memberSplit}</Text>
+                  </View>
+                </View>
+                <TouchableHighlight style={styles.button} onPress={() => navigate('Expense')}>
+                  <Text style={styles.buttonText}>Add Expense</Text>
+                </TouchableHighlight>
+              </View>
+              <MemberList groupId={id} members={this.state.members} navigate={ navigate }/>
+              </View>
+              <View style={styles.footer}>
+                <TouchableHighlight style={styles.settleButton} onPress={this.settleUpHandler}>
+                  <Text style={styles.settleText}>Settle up</Text>
+                </TouchableHighlight>
+              </View>
+          </View>
+        </LinearGradient>
+      )
+    } else if(this.state.group.settledUp === true) {
+      return(
+        <LinearGradient colors={['#b6fbff', '#83a4d4']} style={styles.linearGradient}>
+          <View style={styles.container}>
+            <View>
+              <View>
+                <Text style={styles.groupHeader}>{this.state.group.groupName}</Text>
+                <Text style={styles.details}>{this.state.group.details}</Text>
+              </View>
+              <View style={styles.sumHeader}>
+                <View style={styles.sumContainer}>
+                  <View style={styles.sumBox}>
+                    <Text style={styles.expenseHeader}>Group Spent</Text>
+                    <Text>${this.state.totalSpend}</Text>
+                  </View>
+                  <View style={styles.sumBox}>
+                    <Text style={styles.expenseHeader}>Member Split</Text>
+                    <Text>${this.state.memberSplit}</Text>
+                  </View>
+                </View>
+                <TouchableHighlight style={styles.button} onPress={() => navigate('Expense')}>
+                  <Text style={styles.buttonText}>Add Expense</Text>
+                </TouchableHighlight>
+              </View>
+              <MemberList groupId={id} members={this.state.members} navigate={ navigate }/>
+              </View>
+          </View>
+        </LinearGradient>
+      )
+    }
   }
 }
 
