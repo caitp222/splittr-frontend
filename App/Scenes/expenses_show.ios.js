c992894ddffc37ae2ea76d40c6eb523c6dc75f4e
Rebecca trachtenberg <rebecca.rose1836@yahoo.com>
2017-10-04 09:40:45 -0400

    adding design

--- App/Scenes/expenses_show.ios.js
+++ App/Scenes/expenses_show.ios.js
@@ -3,12 +3,18 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  ImageBackground
 } from 'react-native';
-
+import backgroundImage from '../../blueblack.jpg'
 
 
 class ExpenseShowScene extends Component {
+  static navigationOptions = {
+    title: 'Bills Bills Bills',
+    headerStyle: {backgroundColor: '#F7F9FB'}
+  };
   constructor() {
     super();
     this.state = { expense: {
@@ -37,7 +43,7 @@ class ExpenseShowScene extends Component {
             user: responseJson.paid_by
           }
         })
-      }).catch(err => { 
+      }).catch(err => {
         console.log(err)
       })
   }
@@ -45,14 +51,46 @@ class ExpenseShowScene extends Component {
   render() {
     const { expense } = this.state
     return(
-      <View>
-        <Text>Expense</Text>
-        <Text>Vendor: {expense.vendor}</Text>
-        <Text>Description: {expense.description}</Text>
-        <Text>Amount: ${expense.amount}</Text>
-        <Text>Paid By: {expense.user}</Text>
+      <Image
+      style={styles.backdrop}
+      source={backgroundImage}>
+      <View style={styles.container}>
+        <Text style={styles.header}>Expense</Text>
+        <Text style={styles.text}>Vendor: {expense.vendor}</Text>
+        <Text style={styles.text}>Description:{expense.description}</Text>
+        <Text style={styles.text}>Amount: ${expense.amount}</Text>
+        <Text style={styles.text}>Paid By: {expense.user}</Text>
       </View>
+      </Image>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: "5%",
+    backgroundColor: 'transparent',
+  },
+  backdrop: {
+    width: '100%',
+    height: '100%',
+  },
+  header: {
+    fontSize: 40,
+    paddingBottom: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    color:'#F7F9FB',
+    fontWeight: 'bold',
+  },
+  text:{
+    fontSize: 20,
+    margin: "5%",
+    backgroundColor: 'transparent',
+    color:'#F7F9FB',
+    fontWeight: 'bold',
+  }
+})
+
 export default ExpenseShowScene ;
