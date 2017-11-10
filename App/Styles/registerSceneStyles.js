7a6a7e404527eabc7911b71d5e6460509e2021d8
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-11-10 13:13:27 -0500

    finish stylesheets refactor

---
+++ App/Styles/registerSceneStyles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const RegisterSceneStyles = StyleSheet.create({
+  linearGradient: {
+    height: "100%",
+  },
+  background: {
+    flex: 1,
+    backgroundColor: 'transparent'
+  },
+  button: {
+    backgroundColor: '#00ff00',
+    fontWeight: 'bold',
+  },
+});
+
+export default RegisterSceneStyles;
